941067edecb10023ea0e7eaaa172769b32a6cc46
genuinerick <rcabral@wearegenuine.com>
2017-11-04 19:06:16 -0400

    Work on SupportingItemManagement. Not complete.

---
+++ Constellation.Foundation.SupportingItemManagement/Pipelines/EnsureSupportingItems.cs
@@ -0,0 +1,20 @@
+﻿using Sitecore.Pipelines;
+
+namespace Constellation.Foundation.SupportingItemManagement.Pipelines
+{
+	public class EnsureSupportingItems
+	{
+		public void Process(PipelineArgs args)
+		{
+			Sitecore.Diagnostics.Assert.ArgumentNotNull(args, "args");
+			// Do the things.
+
+			if (args.Aborted)
+			{
+				return; // Aborted
+			}
+
+			// do things
+		}
+	}
+}
