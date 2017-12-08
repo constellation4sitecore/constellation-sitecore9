13a0535cde17a6ce79300157074beb62acb02873
genuinerick <rcabral@wearegenuine.com>
2017-12-08 10:59:45 -0500

    Removed URI support from Image and File to fix a bug.

--- Constellation.Foundation.ModelMapping/FieldMappers/ImageFieldMapper.cs
+++ Constellation.Foundation.ModelMapping/FieldMappers/ImageFieldMapper.cs
@@ -1,35 +1,114 @@
-﻿using Constellation.Foundation.ModelMapping.MappingAttributes;
+﻿using Constellation.Foundation.Data;
+using Constellation.Foundation.ModelMapping.MappingAttributes;
 using Sitecore.Data.Fields;
+using Sitecore.Diagnostics;
 using Sitecore.Resources.Media;
+using Sitecore.Web.UI.WebControls;
 using System;
 using System.Reflection;
 
 namespace Constellation.Foundation.ModelMapping.FieldMappers
 {
-	public class ImageFieldMapper : FieldMapper<Uri>
+	public class ImageFieldMapper : IFieldMapper
 	{
-		protected override string ExtractStringValueFromField()
+		#region Fields
+
+		private string _propertyName = null;
+		private PropertyInfo _property = null;
+		#endregion
+
+		#region Properties
+		protected object Model { get; set; }
+
+		protected Field Field { get; set; }
+
+		protected virtual string PropertyName
+		{
+			get
+			{
+				if (_propertyName == null)
+				{
+					_propertyName = Field.Name.AsPropertyName();
+				}
+
+				return _propertyName;
+			}
+		}
+
+		protected PropertyInfo Property
+		{
+			get
+			{
+				if (_property == null)
+				{
+					_property = Model.GetType().GetProperty(PropertyName, BindingFlags.Instance | BindingFlags.Public);
+				}
+
+				return _property;
+			}
+		}
+
+		#endregion
+
+		public virtual FieldMapStatus Map(object modelInstance, Field field)
+		{
+			Model = modelInstance;
+			Field = field;
+
+			if (Property == null)
+			{
+				return FieldMapStatus.NoProperty;
+			}
+
+			if (Property.GetCustomAttribute<DoNotMapAttribute>() != null)
+			{
+				return FieldMapStatus.ExplicitIgnore;
+			}
+
+			if (Property.GetCustomAttribute<RawValueOnlyAttribute>() != null)
+			{
+				Property.SetValue(Model, Field.Value);
+				return FieldMapStatus.Success;
+			}
+
+			var paramsAttribute = Property.GetCustomAttribute<FieldRendererParamsAttribute>();
+
+			if (paramsAttribute != null)
+			{
+				Property.SetValue(Model, FieldRenderer.Render(Field.Item, Field.Name, paramsAttribute.Params));
+				return FieldMapStatus.Success;
+			}
+
+			// Place to handle more complex scenarios.
+			try
+			{
+				if (PropertyIsString())
+				{
+
+
+					Property.SetValue(Model, ExtractStringValueFromField());
+					return FieldMapStatus.Success;
+				}
+
+				return FieldMapStatus.TypeMismatch;
+			}
+			catch (Exception ex)
+			{
+				Log.Error($"Failed to map Field {Field.Name} of Item {Field.Item.Name}", ex, this);
+				return FieldMapStatus.Exception;
+			}
+		}
+
+		protected string ExtractStringValueFromField()
 		{
 			var urlAttribute = Property.GetCustomAttribute<RenderAsUrlAttribute>();
 
 			if (urlAttribute == null || urlAttribute.UseFieldRendererInEditor && Sitecore.Context.PageMode.IsExperienceEditorEditing)
 			{
-				return base.ExtractStringValueFromField();
+				return FieldRenderer.Render(Field.Item, Field.Name);
 			}
 
 			return GetUrlFromField();
-		}
-
-		protected override Uri ExtractTypedValueFromField()
-		{
-			var url = GetUrlFromField();
-
-			if (!string.IsNullOrEmpty(url))
-			{
-				return new Uri(GetUrlFromField());
-			}
-
-			return null;
 		}
 
 		private string GetUrlFromField()
@@ -42,6 +121,11 @@ namespace Constellation.Foundation.ModelMapping.FieldMappers
 			}
 
 			return string.Empty;
+		}
+
+		protected bool PropertyIsString()
+		{
+			return typeof(string).IsAssignableFrom(Property.PropertyType);
 		}
 	}
 }
