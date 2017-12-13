8b9346c4b9e9e6944bd8a6415c58751f13769cf1
genuinerick <rcabral@wearegenuine.com>
2017-12-13 16:24:48 -0500

    Added default value for NavigationNode.Children

--- Constellation.Feature.Navigation/Models/NavigationNode.cs
+++ Constellation.Feature.Navigation/Models/NavigationNode.cs
@@ -2,10 +2,25 @@
 
 namespace Constellation.Feature.Navigation.Models
 {
+	/// <summary>
+	/// Represents a link to a page in a typical accordion or tree-based navigation scheme.
+	/// </summary>
 	public class NavigationNode : TargetItem
 	{
+		public NavigationNode()
+		{
+			Children = new List<NavigationNode>();
+		}
+
+		/// <summary>
+		/// The children of this page, if this page is an ancestor-or-current
+		/// of the Context Item.
+		/// </summary>
 		public ICollection<NavigationNode> Children { get; set; }
 
+		/// <summary>
+		/// Indicates that this instance is an ancestor-or-current of the Context Item.
+		/// </summary>
 		public bool IsActive { get; set; }
 	}
 }
