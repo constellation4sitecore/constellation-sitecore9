a57e49824faf43925c1470540b0526ca5cb9b4cc
genuinerick <rcabral@wearegenuine.com>
2018-01-11 12:39:18 -0500

    Added mapping for General Link Class attribute.

---
+++ Constellation.Foundation.ModelMapping/FieldMappers/GeneralLinkClassMapper.cs
@@ -0,0 +1,19 @@
+﻿using Sitecore.Data.Fields;
+
+namespace Constellation.Foundation.ModelMapping.FieldMappers
+{
+	public class GeneralLinkClassMapper : FieldAttributeMapper
+	{
+		protected override string GetPropertyName()
+		{
+			return base.GetPropertyName() + "Class";
+		}
+
+		protected override object GetValueToAssign()
+		{
+			LinkField field = Field;
+
+			return field.Class;
+		}
+	}
+}
