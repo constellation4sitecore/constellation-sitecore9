82dcdd29e6edce263d41d9815db86741d394979c
Richard.Cabral@verndale.com <richard.cabral@verndale.com>
2019-05-28 15:49:48 -0400

    Added support for the "Src" suffix on Image Field properties to provide a cleaner way to get at the Image URL.

---
+++ Constellation.Foundation.ModelMapping/FieldMappers/ImageSrcMapper.cs
@@ -0,0 +1,58 @@
+﻿using Sitecore.Data.Fields;
+using Sitecore.Resources.Media;
+
+namespace Constellation.Foundation.ModelMapping.FieldMappers
+{
+	/// <inheritdoc />
+	/// <summary>
+	/// Maps the "Alt" Attribute of an Image field to a Model Property suffixed with the word "Alt".
+	/// </summary>
+	public class ImageSrcMapper : FieldAttributeMapper
+	{
+		/// <inheritdoc />
+		protected override string GetPropertyName()
+		{
+			return base.GetPropertyName() + "Src";
+		}
+
+		/// <inheritdoc />
+		protected override object GetValueToAssign()
+		{
+			return GetUrlFromField();
+		}
+
+		private string GetUrlFromField()
+		{
+			ImageField field = Field;
+			var targetImage = field.MediaItem;
+
+			if (targetImage != null)
+			{
+
+				var options = new MediaUrlOptions()
+				{
+					Language = targetImage.Language
+				};
+
+				int width;
+				if (int.TryParse(field.Width, out width) && width > 0)
+				{
+					options.Width = width;
+				}
+
+				int height;
+				if (int.TryParse(field.Height, out height) && height > 0)
+				{
+					options.Height = height;
+				}
+
+				var innerUrl = MediaManager.GetMediaUrl(targetImage, options);
+				var url = HashingUtils.ProtectAssetUrl(innerUrl);
+
+				return url;
+			}
+
+			return string.Empty;
+		}
+	}
+}
