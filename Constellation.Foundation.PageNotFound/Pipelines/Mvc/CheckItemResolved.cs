704884264e8c9f2855fe8e7cc9e417f917e96299
Roberto Armas <roberto.armas@verndale.com>
2023-12-18 13:04:17 -0500

    [GAFRB-0] Bug fix for GAFRB when setup 404 in wildcard items

---
+++ Constellation.Foundation.PageNotFound/Pipelines/Mvc/CheckItemResolved.cs
@@ -0,0 +1,19 @@
+﻿using Sitecore;
+using Sitecore.Mvc.Pipelines;
+using Sitecore.Mvc.Pipelines.Response.GetPageItem;
+
+namespace Constellation.Foundation.PageNotFound.Pipelines.Mvc
+{
+    public class CheckItemResolved : MvcPipelineProcessor<GetPageItemArgs>
+    {
+        public override void Process(GetPageItemArgs args)
+        {
+            var resolved = Sitecore.Context.Items["constellation4sitecore.Foundation.PageNotFound::ItemNotFoundResolved"];
+            if (MainUtil.GetBool(resolved, false))
+            {
+                // item has previously been resolved
+                args.Result = Sitecore.Context.Item;
+            }
+        }
+    }
+}
